727b8d498af1645a77c7150c85152a7ca971b9fc
mijungmoon <69185354+mijungmoon@users.noreply.github.com>
2020-08-05 16:52:58 +0900

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,6 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, Hello, world");
-		System.out.println("bye, world");
+		System.out.println("bye, bye, world");
 	}
 }
